871352ba32aa434ed72c65470bc0675a041bb8cb
Kyle Sullivan <kylesullivan123@gmail.com>
2021-09-01 15:26:02 -0700

    commit

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,8 +1,7 @@
-import { render } from '@testing-library/react'
 import React, {Component} from 'react'
 
 
-class PlayerCard extends React.Component {
+class PlayerCard extends Component {
     constructor (props) {
         super(props)
         this.state = {
@@ -21,11 +20,12 @@ class PlayerCard extends React.Component {
 
 
     render(){
+        const{last_name, first_name, hair_color} = this.props
         return(
             <div>
-                <h1>{this.props.last_name}, {this.props.first_name}</h1>
+                <h1>{last_name}, {first_name}</h1>
                 <p>Age: {this.state.age}</p>
-                <p>Hair Color: {this.props.hair_color}</p>
+                <p>Hair Color: {hair_color}</p>
                 <button onClick={this.birthdayButton}>Birthday Button for {this.props.first_name} {this.props.last_name}</button>
             </div>
         )
